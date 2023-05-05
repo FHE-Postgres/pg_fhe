7f79ecfec6caebf732a4d5214607b8af462c9e7a
W-Dominic <dom0_o@protonmail.com>
2023-05-05 16:13:15 -0400

    works a little better

--- example_ext/pg_cpp/influx_cpp.cpp
+++ example_ext/pg_cpp/influx_cpp.cpp
@@ -1,5 +1,7 @@
 // Declare c++ libraries here
 #include <openssl/sha.h>
+#include <iostream>
+#include <cstdio>
 
 extern "C" {
     // Declare C libraries here
@@ -49,14 +51,19 @@ extern "C" {
 
     Datum influx_sha(PG_FUNCTION_ARGS) {
     char *ptext; // plaintext input
-    char ctext[SHA256_DIGEST_LENGTH + 1]; // ciphertext (hashed) output
+    char ctext[SHA256_DIGEST_LENGTH]; // ciphertext (hashed) output
+    char ctext_str[SHA256_DIGEST_LENGTH * 2];
 
+    // should be \0 terminated... i think
 	ptext = text_to_cstring(PG_GETARG_TEXT_PP(0));
 	
 	SHA256((unsigned char*) ptext, strlen(ptext), (unsigned char*) ctext);
-	ctext[SHA256_DIGEST_LENGTH] = '\0';
     
+    // convert to readable str
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++){
+        sprintf(ctext_str + 2 * i, "%02x", ctext[i]);
+    }
     // return pointer to ciphertext
-	PG_RETURN_TEXT_P(cstring_to_text(&ctext[0]));
+	PG_RETURN_TEXT_P(cstring_to_text(&ctext_str[0]));
     }
 } 
