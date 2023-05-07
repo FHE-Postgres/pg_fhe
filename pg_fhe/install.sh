1abbf38bdd43821cff54998c9d968f34d30ec0f5
W-Dominic <dom0_o@protonmail.com>
2023-05-07 01:28:45 -0400

    better install script

--- pg_fhe/install.sh
+++ pg_fhe/install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# cmake and build
+mkdir build
 cd build
-
 cmake ..
-
 make
-
 cd ..
 
+# installing to postgres
 sudo cp ./build/pg_fhe.so $(pg_config --pkglibdir)
-
 sudo cp ./sql/pg_fhe--0.1.sql $(pg_config --sharedir)/extension/
-
 sudo cp ./sql/pg_fhe.control $(pg_config --sharedir)/extension/
+
+# cleanup
+rm -rf build
 
 echo " "
 echo "--- --- --- --- ---"
