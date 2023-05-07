6dfd7b3af2da7b05957b3fecfd87ae52797aeea8
W-Dominic <dom0_o@protonmail.com>
2023-05-07 14:59:42 -0400

    client ckks

--- client/src/fhe_client.cpp
+++ client/src/fhe_client.cpp
@@ -9,10 +9,34 @@ int main() {
     size_t poly_modulus_degree = 8192;
     params.set_poly_modulus_degree(poly_modulus_degree);
     params.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {50, 30, 50}));
-
     SEALContext context(params);
+    /*
     print_parameters(context);
     cout << endl;
+    */
+    KeyGenerator keygen(context);
+    auto sk = keygen.secret_key();
+    PublicKey pk;
+    keygen.create_public_key(pk);
+    
+    stringstream data_stream;
+
+    // Relinearization Keys
+    Serializable<RelinKeys> rlk = keygen.create_relin_keys();
+    auto size_rlk = rlk.save(data_stream);
+
+    // Encrypt
+    double scale = pow(2.0, 30);
+    CKKSEncoder encoder(context);
+    Plaintext plain1, plain2;
+    encoder.encode(2.3, scale, plain1);
+    encoder.encode(4.5, scale, plain2);
+    Encryptor encryptor(context, pk);
+    encryptor.set_secret_key(sk); // set to secret key mode for size optimization
+    auto size_encrypted = encryptor.encrypt_symmetric(plain2).save(data_stream);
+
+    // save datastream to file
+    // TODO
 
     return 0;
 }
