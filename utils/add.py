f8da5725c7289ec8ca3c213462c0daf92b695d5b
W-Dominic <dom0_o@protonmail.com>
2023-05-08 13:46:01 -0400

    cleanup binaries

--- utils/add.py
+++ utils/add.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-insert_id = input("id: ")
+insert_id = int(input("id: "))
 ctext_file = input("ciphertext file: ")
 # Read the binary file
 with open(f"{ctext_file}", "rb") as file:
@@ -10,7 +10,10 @@ with open(f"{ctext_file}", "rb") as file:
 conn = psycopg2.connect("dbname=testdb user=dom")
 
 # Insert the binary data
-with conn.cursor() as cursor:
-    cursor.execute(f"INSERT INTO fitness (user_id, minutes) VALUES ({insert_id}, %s)", (psycopg2.Binary(binary_data),))
-    conn.commit()
+for i in range(10000):
+    insert_id += 1
+    id_str = str(insert_id)
+    with conn.cursor() as cursor:
+        cursor.execute(f"INSERT INTO test (user_id, data) VALUES ({id_str}, %s)", (psycopg2.Binary(binary_data),))
+        conn.commit()
 
