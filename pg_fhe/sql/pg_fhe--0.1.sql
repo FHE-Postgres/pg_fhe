6c9e276cfc2e0934b234ee890e4fb2f303fc0263
W-Dominic <dom0_o@protonmail.com>
2023-05-10 14:18:59 -0400

    pilot

--- pg_fhe/sql/pg_fhe--0.1.sql
+++ pg_fhe/sql/pg_fhe--0.1.sql
@@ -1,4 +1,5 @@
 \echo Use "CREATE EXTENSION pg_fhe" to load this file. \quit
 
+\echo TODO add extra integer parameter to ckks_mult \quit
 CREATE FUNCTION ckks_mult(bytea)
 RETURNS bytea LANGUAGE C AS 'MODULE_PATHNAME';
