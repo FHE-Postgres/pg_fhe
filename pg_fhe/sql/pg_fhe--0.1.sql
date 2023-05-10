1bf501643bebfaed199907aa71690f73b4f3f665
W-Dominic <dom0_o@protonmail.com>
2023-05-10 16:31:02 -0400

    removed todo comment

--- pg_fhe/sql/pg_fhe--0.1.sql
+++ pg_fhe/sql/pg_fhe--0.1.sql
@@ -1,5 +1,4 @@
 \echo Use "CREATE EXTENSION pg_fhe" to load this file. \quit
 
-\echo TODO add extra float parameter to ckks_mult \quit
 CREATE FUNCTION ckks_mult(bytea, float)
 RETURNS bytea LANGUAGE C STRICT AS 'MODULE_PATHNAME';
